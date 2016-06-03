f0aa33b153f75fb4e47c498b39d909206a25c2b2
Brent Martin <bmart35@gmail.com>
2016-06-02 23:30:30 -0500

    removed unnecessary new and edit

--- app/controllers/API/users_controller.rb
+++ app/controllers/API/users_controller.rb
@@ -9,19 +9,7 @@ class Api::UsersController < ApplicationController
     render json: @user.to_json(include: :posts)
   end
 
-  def new
-    # @user = User.new                                    # in controllers
-    render json: @user = User.new
-  end
-
   def create
-    # @user = User.new                                    # in controllers
-    # @user.save                                          # in controllers
-    render json: @user = User.new(username: params[:user][:username])
-  end
-
-  def edit
-    # @user = User.find(params[:id])                      # in controllers
     render json: @user
   end
 
