565a960c99300cfdf7e20f9dcc162bbb05acc65e
Brent Martin <bmart35@gmail.com>
2016-06-02 23:27:55 -0500

    removed unnecessary set_user def

--- app/controllers/API/users_controller.rb
+++ app/controllers/API/users_controller.rb
@@ -43,10 +43,6 @@ class Api::UsersController < ApplicationController
 
 private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
-
   def user_params
     params.require(:user).permit(:name)
   end
