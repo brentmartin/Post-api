7331e68a0ae1ee5468975b0f0532539b08b4eaf6
Brent Martin <bmart35@gmail.com>
2016-06-02 23:32:27 -0500

    removed resources :posts and created a namespace resource for posts api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts
   namespace :api do
     resources :users, only: [:index, :show, :create, :update, :destroy]
+    resources :posts, only: [:index, :show, :create, :update, :destroy]
   end
 
 end
