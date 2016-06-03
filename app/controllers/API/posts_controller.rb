39f9936f6b17794737c541ccdf7623658ec16c22
Brent Martin <bmart35@gmail.com>
2016-06-02 23:39:05 -0500

    removed comment-outs for index, show, update, and destroy

--- app/controllers/API/posts_controller.rb
+++ app/controllers/API/posts_controller.rb
@@ -2,20 +2,20 @@ class Api::PostsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    # @posts = Post.all                                   # in controllers
+    @posts = Post.all
     render json: @posts
   end
 
   def show
-    # @post = Post.find(param[:id])                       # in controllers
     render json: @post
   end
 
+    @post = Post.find(params[:id])
   end
 
   def create
-    # @post = Post.new                                    # in controllers
     @post.body = params[:post][:body]
+    @post = Post.new(post_params)
     @post.save
     render json: @post = Post.new(post = params[:post][:body])
   end
@@ -24,15 +24,14 @@ class Api::PostsController < ApplicationController
   end
 
   def update
-    # @post = Post.find(param[:id])                       # in controllers
+    @post = Post.find(params[:id])
     @post.update(post_params)
     rescue ActiveRecord::RecordNotFound
        render json: { message: "Not Found", status: 404}, status: 404
   end
 
   def destroy
-    # @post = Post.find(param[:id])                       # in controllers
-    # @post.destroy                                       # in controllers
+    @post = Post.find(params[:id])
     @post.destroy
     render json: @post.destroy
     rescue ActiveRecord::RecordNotFound
